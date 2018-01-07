1e706536b9153b6e96da551d0baeaed6ebf0879b
CasualCoding A.K.A Rester71 <rester7201@gmail.com>
2018-01-06 21:31:54 -0600

    Programas de if-else

---
+++ Estructuras de decision/programa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	float calif;
+	printf("Da la calificacion de tu examen: \n");
+	scanf("%f",&calif);
+	
+	if( calif >= 6 )
+		printf("Has sido aceptado!!! :)");
+	else
+		printf("No has sido aceptado!!! :(");
+		
+	
+		
+	
+	
+	
+	return 0;
+}
+
